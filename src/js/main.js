e849622d0686cb8d7593b9b553805ef71e7bec33
Elvira Cañas <elvfira@gmail.com>
2021-12-08 23:44:22 +0100

    changes js

--- src/js/main.js
+++ src/js/main.js
@@ -4,12 +4,21 @@ console.log('>> Ready :)');
 
 
 // inhablitar botón cuando se ha clicado
+// y mostrar div de compartir tarjeta cuando se haya creado
 
 const cardButton = document.querySelector('#button-card')
+const shareDiv = document.querySelector('#share-card')
 
 function unabling () {
     cardButton.classList.add('unabled');
+    shareDiv.classList.remove('hidden');
 }
 
 cardButton.addEventListener('click', unabling)
+
+
+
+
+
+
 
